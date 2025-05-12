b29e6357e67a8f789866b25cf604fff2369669e6
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-12 10:39:32 -0300

    Start do projeto

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.service.Api;
+import br.com.alura.screenmatch.service.ConvertData;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var apiService = new Api();
+		var convert = new ConvertData();
+		var json = apiService.getData("https://www.omdbapi.com/?t=gilmore+girls&apikey=90ca39e7");
+		System.out.println(json);
+		var serie = convert.getData(json, Serie.class);
+		System.out.println(serie);
+	}
+}
