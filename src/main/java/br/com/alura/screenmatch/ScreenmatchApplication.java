59f0ebce668a16bbc66fb2b041d8ebda58b3cb07
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-12 12:12:53 -0300

    Buscando dados completos da serie

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,9 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.Episode;
-import br.com.alura.screenmatch.model.Season;
-import br.com.alura.screenmatch.model.Serie;
-import br.com.alura.screenmatch.service.Api;
-import br.com.alura.screenmatch.service.ConvertData;
+import br.com.alura.screenmatch.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,23 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var apiService = new Api();
-		var convert = new ConvertData();
-		var json = apiService.getData("https://www.omdbapi.com/?t=gilmore+girls&apikey=90ca39e7");
-		System.out.println(json);
-		var serie = convert.getData(json, Serie.class);
-		System.out.println(serie);
-
-		json = apiService.getData("https://www.omdbapi.com/?t=gilmore+girls&apikey=90ca39e7&season=1&episode=2");
-		Episode episode = convert.getData(json, Episode.class);
-		System.out.println(episode);
-
-		List<Season> seasonList = new ArrayList<>();
-		for (int i = 1; i <= serie.totalSeasons(); i++) {
-			json = apiService.getData("https://www.omdbapi.com/?t=gilmore+girls&apikey=90ca39e7&season=" + i);
-			Season season = convert.getData(json, Season.class);
-			seasonList.add(season);
-		}
-		seasonList.forEach(System.out::println);
+		Main main = new Main();
+		main.showSerie();
 	}
 }
---
+++ src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -0,0 +1,35 @@
+package br.com.alura.screenmatch.main;
+
+import br.com.alura.screenmatch.model.Season;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.service.Api;
+import br.com.alura.screenmatch.service.ConvertData;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private final String URL = "https://www.omdbapi.com/?t=";
+    private final String APIKEY = "&apikey=90ca39e7";
+    private final Scanner scanner = new Scanner(System.in);
+    private final Api api = new Api();
+    private final ConvertData convertData = new ConvertData();
+
+    public void showSerie() {
+        System.out.println("Digite o nome da serie: ");
+        var titleSerie = scanner.nextLine();
+        var json = api.getData(URL + titleSerie.replace(" ", "+") + APIKEY);
+        var serie = convertData.getData(json, Serie.class);
+        System.out.println(serie);
+
+        List<Season> seasonList = new ArrayList<>();
+        for (int i = 1; i <= serie.totalSeasons(); i++) {
+            json = api.getData(URL + titleSerie.replace(" ", "+") + APIKEY + "&season=" + i);
+            Season season = convertData.getData(json, Season.class);
+            seasonList.add(season);
+        }
+        seasonList.forEach(System.out::println);
+    }
+}
