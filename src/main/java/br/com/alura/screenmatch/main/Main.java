b32f5df58f1f1bca2238b129aa3b8f65cdc2efe7
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-14 10:30:38 -0300

    Exibindo as etapas de uma stream

--- src/main/java/br/com/alura/screenmatch/main/Main.java
+++ src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -29,7 +29,7 @@ public class Main {
         var titleSerie = scanner.nextLine();
         var json = api.getData(URL + titleSerie.replace(" ", "+") + APIKEY);
         var serie = convertData.getData(json, SerieData.class);
-        System.out.println(serie);
+//        System.out.println(serie);
 
         List<SeasonData> seasonDataList = new ArrayList<>();
         for (int i = 1; i <= serie.totalSeasons(); i++) {
@@ -37,7 +37,7 @@ public class Main {
             SeasonData seasonData = convertData.getData(json, SeasonData.class);
             seasonDataList.add(seasonData);
         }
-        seasonDataList.forEach(System.out::println);
+//        seasonDataList.forEach(System.out::println);
 
         seasonDataList.forEach(s -> s.episodeData().forEach(e -> System.out.println(e.title())));//lambdas no java
 
@@ -45,31 +45,36 @@ public class Main {
                 .flatMap(s -> s.episodeData().stream())
                 .toList();
 
-        System.out.println("\n Top cinco episodios");
+        System.out.println("\n Top 10 episodios");
         episodeDataList.stream()
                 .filter(e -> !e.avaliation().equalsIgnoreCase("N/A"))
+                .peek(e -> System.out.println("Primeiro filtro(N/A) " + e))
                 .sorted(Comparator.comparing(EpisodeData::avaliation).reversed())
-                .limit(5)
+                .peek(e -> System.out.println("Ordenacao " + e))
+                .limit(10)
+                .peek(e -> System.out.println("Limitacao " + e))
+                .map(e -> e.title().toUpperCase())
+                .peek(e -> System.out.println("Mapeamento " + e))
                 .forEach(System.out::println);
 
-        List<Episode> episodeList = seasonDataList.stream()
-                .flatMap(s -> s.episodeData().stream()
-                        .map(e -> new Episode(s.number(), e)))
-                .collect(Collectors.toList());
-        System.out.println(episodeList);
-
-        System.out.println("A partir de qual ano voce deseja ver os episodios? ");
-        var year = scanner.nextInt();
-        scanner.nextLine();
-
-        LocalDate searchDate = LocalDate.of(year, 1, 1);
-
-        episodeList.stream()
-                .filter(e -> e.getReleasedDate() != null && e.getReleasedDate().isAfter(searchDate))
-                .forEach(e -> System.out.println(
-                        "Temporada: " + e.getSeason() +
-                                " Episodio: " + e.getTitle() +
-                                " Data lancamento: " + e.getReleasedDate().format(formatter)
-                ));
+//        List<Episode> episodeList = seasonDataList.stream()
+//                .flatMap(s -> s.episodeData().stream()
+//                        .map(e -> new Episode(s.number(), e)))
+//                .collect(Collectors.toList());
+//        System.out.println(episodeList);
+//
+//        System.out.println("A partir de qual ano voce deseja ver os episodios? ");
+//        var year = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        LocalDate searchDate = LocalDate.of(year, 1, 1);
+//
+//        episodeList.stream()
+//                .filter(e -> e.getReleasedDate() != null && e.getReleasedDate().isAfter(searchDate))
+//                .forEach(e -> System.out.println(
+//                        "Temporada: " + e.getSeason() +
+//                                " Episodio: " + e.getTitle() +
+//                                " Data lancamento: " + e.getReleasedDate().format(formatter)
+//                ));
     }
 }
