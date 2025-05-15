1ed274c5a5990b4b52db45b0031c33d8cbea6eb9
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-15 14:01:40 -0300

    Evoluindo nossa aplicacao Java

--- src/main/java/br/com/alura/screenmatch/main/Main.java
+++ src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -16,36 +16,45 @@ public class Main {
     private final Api api = new Api();
     private final ConvertData convertData = new ConvertData();
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private List<SerieData> serieData = new ArrayList<>();
 
     public void showMenu() {
-        var menu = """
+        var option = -1;
+        while (option != 0) {
+            var menu = """
                 1 - Buscar series
                 2 - Buscar episódios
+                3 - Listar series
                 
                 0 - sair
                 """;
-        System.out.println(menu);
+            System.out.println(menu);
 
-        var option = scanner.nextInt();
-        scanner.nextLine();
+            option = scanner.nextInt();
+            scanner.nextLine();
 
-        switch (option) {
-            case 1:
-                searchSerieWeb();
-                break;
-            case 2:
-                searchEpFromSerie();
-                break;
-            case 0:
-                System.out.println("Encerrando.....");
-                break;
-            default:
-                System.out.println("Opcao invalida");
+            switch (option) {
+                case 1:
+                    searchSerieWeb();
+                    break;
+                case 2:
+                    searchEpFromSerie();
+                    break;
+                case 3:
+                    listSearchedSeries();
+                    break;
+                case 0:
+                    System.out.println("Encerrando.....");
+                    break;
+                default:
+                    System.out.println("Opcao invalida");
+            }
         }
     }
 
     private void searchSerieWeb() {
         SerieData data = getSerieData();
+        serieData.add(data);
         System.out.println(data);
     }
 
@@ -68,5 +77,9 @@ public class Main {
         }
 
         seasonDataList.forEach(System.out::println);
+    }
+
+    private void listSearchedSeries() {
+        serieData.forEach(System.out::println);
     }
 }
